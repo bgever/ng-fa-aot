35c336677d5c2582b85a60620ee5440f19af02fa
Bart Verkoeijen <bart.verkoeijen@gmail.com>
2019-12-16 10:50:36 +0800

    move fontawesome import to shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
